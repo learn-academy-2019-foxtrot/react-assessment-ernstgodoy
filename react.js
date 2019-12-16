4b94cd9b58ebcbf6ec719c184dc623567515db10
Ubuntu <ubuntu@ip-172-31-39-9.us-east-2.compute.internal>
2019-12-16 16:59:38 +0000

    completed assessment

--- react.js
+++ react.js
@@ -5,6 +5,27 @@
 
 // 1. Write a React component that prints "I am a component!" Be sure to include all necessary imports, exports, etc.
 
+import React, { Component } from 'react';
+
+class Greeting extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      greeting : "I am a component!"
+    }
+  }
+  
+  render() {
+    return(
+      <div>
+      {this.state.greeting}
+      </div>
+  )}
+
+
+}
+
+export default Greeting
 
 
 
@@ -18,6 +39,20 @@ for(let i=0; i<names.length; i++){
 }
 
 
+const howMany = (array) =>{
+  array.map((value, index) => 
+  console.log(`${names[index]} is ${names[index].length} characters long.`)
+  )
+}
+
+howMany(names)
+
+
+// names.map((v,i)=>{
+// console.log(`${names[i]} is ${names[i].length} characters long.`)
+// })
+
+
 
 // 3. Destructure the following variables out of state.
 
@@ -27,6 +62,12 @@ this.state = {
   dislikes: ["mirrors", "garlic", "wooden stakes"]
 }
 
+var {name , home , dislikes} = this.state
+ 
+console.log(name)
+console.log(home)
+console.log(dislikes)
+
 
 
 // 4. Write a React method that would add one and update the state of the count each time the method is called.
@@ -35,29 +76,31 @@ this.state = {
   count: 0
 }
 
+const add1 = () => {
+  this.setState({count : this.state.count + 1})
+}
 
 
 // 5. There are four mistakes in this code that would cause it to break our application. Find the mistakes and fix them:
 
 import React, { Component } from 'react';
 
-class Recipes{
+class Recipes extends Component{
   constructor(props){
     super(props)
     this.state = {
-      recipes:
-        {name: 'Meatballs'},
-        {name: 'Mac & Cheese'}
+      recipes:[ {name: 'Meatballs'}, {name: 'Mac & Cheese'} ]
     }
   }
 
   render() {
-    return(
+      let {recipes} = this.state
       let recipe = recipes.map(recipe => {
         return(
           <li key={recipe.name}>{recipe.name}</li>
         )
       })
+    return(
       <ul>
         {recipe}
       </ul>
